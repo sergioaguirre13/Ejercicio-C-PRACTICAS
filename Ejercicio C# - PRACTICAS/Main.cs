daf6846888863fc8cd25108df3ccad13d32d36bd
sergioaguirre13 <sergio.aguirre@bue.edu.ar>
2024-06-05 12:48:57 -0300

    first commit

---
+++ Ejercicio C# - PRACTICAS/Main.cs
@@ -0,0 +1,42 @@
+﻿namespace Ejercicio_C____PRACTICAS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+           
+
+            Dog dog = new Dog();
+            dog.Saludar();
+
+            Cat cat = new Cat();
+            cat.Saludar();
+
+
+        }
+
+        public abstract class Animal
+        {
+            public abstract void Saludar();
+            
+        }
+
+        public class Dog : Animal
+        {
+            public override void Saludar()
+            {
+                Console.WriteLine("Hola soy un perro");
+            }
+        }
+
+        public class Cat : Animal
+        {
+            public override void Saludar()
+            {
+                Console.WriteLine("Miau");
+            }
+        }
+    }
+}
---
+++ EjercicioPolimorfismo/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace EjercicioPolimorfismo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var shapes = new List<Shape>();
+            var rectangle = new Rectangle() { Width = 20, Height = 10 };
+            var triangle = new Triangle() { Width = 10, Height = 15 };
+            var square = new Square() { Width = 10 };
+
+            shapes.Add(rectangle);
+            shapes.Add(triangle);
+            shapes.Add(square);
+
+
+            foreach (var item in shapes)
+            {
+                Console.WriteLine(item.CalculateArea());
+            }
+
+
+
+
+        }
+
+
+
+
+
+        public abstract class Shape
+        {
+            public int Height { get; set; }
+            public int Width { get; set; }
+
+            public abstract double CalculateArea();
+        }
+
+        public class Square : Shape
+        {
+            public override double CalculateArea()
+            {
+                return Math.Pow(Width, 2);
+            }
+        }
+
+        public class Rectangle : Shape
+        {
+            public override double CalculateArea()
+            {
+                return Width * Height;
+            }
+        }
+
+        public class Triangle : Shape
+        {
+            public override double CalculateArea()
+            {
+                return (Width * Height) / 2;
+            }
+        }
+    }
+}
